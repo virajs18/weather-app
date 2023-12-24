572b1a8dcac6637d5d2d7dfafe1c0fa04253ad63
virajs18 <140260578+virajs18@users.noreply.github.com>
2023-12-24 09:30:48 +0530

    index.js

--- weather/index.js
+++ weather/index.js
@@ -1,32 +1,28 @@
 var inputvalue = document.querySelector('#cityinput');
 var btn = document.querySelector('#add');
-var city = document.querySelector('#cityoutput')
-var descrip = document.querySelector('#description')
-var temp = document.querySelector('#temp')
-var wind = document.querySelector('#wind')
-apik ='1881b8eda2e35046f5f67e0754cd9482'
-function convertion(val)
-{
-    return (val -273).toFixed(3)
+var city = document.querySelector('#cityoutput'); 
+var description = document.querySelector('#description'); 
+var temp = document.querySelector('#temp');
+var wind = document.querySelector('#wind');
+var apik = '1881b8eda2e35046f5f67e0754cd9482';
+
+function convertion(val) {
+    return (val - 273).toFixed(3);
 }
-btn.addEventListener('click', function()
-{
-fetch('https://api.openweathermap.org/data/2.5/weather?q='+inputvalue.value+'&appid='+apik)
-.then(res => res.json())
 
-.then(data =>
-    {
-        var nameval = data['name']
-        var descrip = data['weather']['0']['description']
-        var tempature= data['main']['temp']
-        var windspeed = data['wind']['speed']
+btn.addEventListener('click', function () {
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputvalue.value + '&appid=' + apik)
+        .then(res => res.json())
+        .then(data => {
+            var nameval = data['name'];
+            var descrip = data['weather'][0]['description']; 
+            var temperature = data['main']['temp']; 
+            var windspeed = data['wind']['speed'];
 
-
-
-        city.innerHTML=`Weatherof<span>${nameval}<span>`
-        temp.innerHTML=`Temperature: <span>${ convertion(temperarture)} C</s` 
-        description.innerHTML= `Sky Conditions: <span>${descrip}<span>`
-        wind.innerHTML =`Wind speed: <span>${windspeed} km/h<span>`
-    })
-    .catch(err =>alert('You entered Wrong city name'))
-})
+            city.innerHTML = `Weather of <span>${nameval}</span>`; 
+            temp.innerHTML = `Temperature: <span>${convertion(temperature)} C</span>`; 
+            description.innerHTML = `Sky Conditions: <span>${descrip}</span>`; 
+            wind.innerHTML = `Wind speed: <span>${windspeed} km/h</span>`; 
+        })
+        .catch(err => alert('You entered the wrong city name'));
+});
